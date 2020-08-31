2bb0bc45b6b1c3fa43efc450a276b0e6f463dff7
Matt <m23.cullen@gmail.com>
2020-08-31 18:40:58 -0400

    fixed compile time, added InitGame function

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,12 +4,31 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    // Welcome the player
     PrintLine(TEXT("Hello"));
+    PrintLine(TEXT("Guess the four letter word"));
     PrintLine(TEXT("press enter to begin"));
+
+    InitGame(); // Setting up the game
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("cake");
+    
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("You win"));
+    }
+    else 
+    {
+        PrintLine(TEXT("You lose"));
+    }
+}
+
+void UBullCowCartridge::InitGame()
+{
+    HiddenWord = TEXT("cake"); // Set the hidden word
+    lives = // Set lives
 }
